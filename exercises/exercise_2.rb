719cc348debfce11255c0c33d3a96d21b19a77fa
Clement Shum <clement.shum128@gmail.com>
2023-06-27 22:52:42 -0400

    updating the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+puts "Store1 Name: #{@store1.name}"
+@store2 = Store.find(2)
+@store1.update(name: 'Clement')
+puts "Updated Store1 Name: #{@store1.name}"
