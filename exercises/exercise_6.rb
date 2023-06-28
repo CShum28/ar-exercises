2de9fc689afcdf1f12f50481de8ccb2d82ac8a5b
Clement Shum <clement.shum128@gmail.com>
2023-06-28 18:11:30 -0400

    updated employee hourly pay

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,6 +12,6 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store2.employees.create(first_name: "Bob", last_name: "Dylan", hourly_rate: 80)
 @store4.employees.create(first_name: "Kyle", last_name: "Rainer", hourly_rate: 50)
-@store5.employees.create(first_name: "Ngyuen", last_name: "Chrisy", hourly_rate: 18)
+@store5.employees.create(first_name: "Ngyuen", last_name: "Chrisy", hourly_rate: 180)
 @store6.employees.create(first_name: "Lanny", last_name: "Walker", hourly_rate: 55)
 
