994c4b7044f1cc200f70be36b379c885964c0193
Clement Shum <clement.shum128@gmail.com>
2023-06-27 23:09:00 -0400

    calculations - sum / average / stores over 1000000

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum("annual_revenue")
+puts total
+
+average = Store.average("annual_revenue")
+puts average
+
+@store_over_1_million = Store.where('annual_revenue > 1000000').count
+puts @store_over_1_million
