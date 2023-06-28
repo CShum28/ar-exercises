4ce978b4d15ffa01d1c4175517983db736fc713f
Clement Shum <clement.shum128@gmail.com>
2023-06-27 23:10:33 -0400

    calculations - sum / average / stores over 1000000

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -15,4 +15,4 @@ average = Store.average("annual_revenue")
 puts average
 
 @store_over_1_million = Store.where('annual_revenue > 1000000').count
-puts @store_over_1_million
+puts "The number of stores making over 1 million is: #{@store_over_1_million}"
