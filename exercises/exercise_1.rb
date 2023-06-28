511022c5a5340bed5625d577f4ba30c63006239f
Clement Shum <clement.shum128@gmail.com>
2023-06-27 22:52:24 -0400

    creating 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+first = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+second = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+third = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "The number of stores created are: #{Store.count}"
